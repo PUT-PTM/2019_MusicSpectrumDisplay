1605908ff16d2829db33ddca4d51a79a3715a91a
ReQezeR <mp62639@o2.pl>
2019-04-01 21:00:51 +0200

    led_tape
    +led_tape files

---
+++ WS_Projekt/Inc/led_tape.h
@@ -0,0 +1,12 @@
+/*
+ * led_tape.h
+ *
+ *  Created on: 01.04.2019
+ *      Author: ReQezeR
+ */
+
+#ifndef LED_TAPE_H_
+#define LED_TAPE_H_
+
+volatile int color1 [];
+#endif /* LED_TAPE_H_ */
---
+++ WS_Projekt/Src/led_tape.c
@@ -0,0 +1,9 @@
+/*
+ * led_tape.c
+ *
+ *  Created on: 01.04.2019
+ *      Author: ReQezeR
+ */
+#include "led_tape.h"
+
+color1 = {0,0,1,1,1,0,0,1,0,0,1,1,1,0,0,1,1,0,1,0,1,0,1,0};
